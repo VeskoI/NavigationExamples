2852bad002530b43c6e19dfaf78800cc65c0e503
Vesko Iliev <iliev.vesselin@gmail.com>
2018-08-08 00:11:05 +0100

    Initial commit - add an empty project with bottom navigation and a few dummy fragments

---
+++ app/src/main/java/com/example/vesseliniliev/navigationexample/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.vesseliniliev.navigationexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavHost
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), NavHost {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        NavigationUI.setupWithNavController(bottom_navigation_view, navController)
+    }
+
+    override fun getNavController() = Navigation.findNavController(this, R.id.navigation_host)
+
+    override fun onSupportNavigateUp() = navController.navigateUp()
+}
